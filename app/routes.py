54cfa82a87f7cc9e3419e91ef628a63f4ac4e5f9
Varun <varuntumbe1@gmail.com>
2020-08-29 15:09:00 +0530

    finished

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ def convert_to_dic(st_obj):
     st_dic['year']=st_obj.year
     return st_dic
 
-def obj_to_dic(pos_obj):
+def obj_to_dic(pos_obj,name,year):
     # pos_dict=dict()
     # pos_dict['lattitude']=pos_obj.lat
     # pos_dict['longitude']=pos_obj.longi
@@ -19,6 +19,8 @@ def obj_to_dic(pos_obj):
     pos_list.append(pos_obj.lat)
     pos_list.append(pos_obj.longi)
     pos_list.append(pos_obj.pressure)
+    pos_list.append(name)
+    pos_list.append(year)
     return pos_list
 
 #all routes
@@ -63,13 +65,41 @@ def years_to_location(year):
     try:
         qobj=Storm.query
         fil_qobj=qobj.filter(Storm.year == int(year)).first()
+        storm_name=fil_qobj.name
+        storm_year=fil_qobj.year
         li=fil_qobj.pos
-        li_dic= [obj_to_dic(obj) for obj in li]
+        li_dic= [obj_to_dic(obj,storm_name,storm_year) for obj in li]
+    #    li_dic= [li.append(storm_name) for li in li_dic]
         #writing data to where.js
         with open('./map_visualization/where.js','w') as fobj:
             fobj.write('myData = ')
             fobj.write(json.dumps(li_dic))
         webbrowser.open_new_tab('file:///F:/flask-api/map_visualization/where.html')
         return json.dumps(li_dic)
+    except Exception as err:
+        return str(err)
+
+
+@app.route('/year:from=<year1>/year:to=<year2>')
+def fromy_to_toy(year1,year2):
+    year1=int(year1)
+    year2=int(year2)
+    try:
+        final_list=list()
+        for i in range(year1,year2):
+            qobj=Storm.query
+            fil_qobj=qobj.filter(Storm.year == int(i)).first()
+            storm_year=fil_qobj.year
+            storm_name=fil_qobj.name
+            li=fil_qobj.pos
+            li_dic= [obj_to_dic(obj,storm_name,storm_year) for obj in li]
+            for li in li_dic:
+                final_list.append(li)
+                #writing data to where.js
+        with open('./map_visualization/where.js','w') as fobj:
+            fobj.write('myData = ')
+            fobj.write(json.dumps(final_list))
+        webbrowser.open_new_tab('file:///F:/flask-api/map_visualization/where.html')
+        return json.dumps(final_list)
     except Exception as err:
         return str(err)
